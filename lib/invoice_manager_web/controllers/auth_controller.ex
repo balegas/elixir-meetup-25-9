bba63747de73f9ed71b5d7ddad94c81beeddf9ca
Fly Dev <dev@fly.io>
2025-09-13 08:51:25 +0000

    fix auth controller login handling

--- lib/invoice_manager_web/controllers/auth_controller.ex
+++ lib/invoice_manager_web/controllers/auth_controller.ex
@@ -1,6 +1,21 @@
 defmodule InvoiceManagerWeb.AuthController do
   use InvoiceManagerWeb, :controller
 
+  def login(conn, %{"password" => password}) do
+    admin_password = System.get_env("ADMIN_PASSWORD") || "admin123"
+
+    if password == admin_password do
+      conn
+      |> put_session(:authenticated, true)
+      |> put_flash(:info, "Successfully logged in!")
+      |> redirect(to: "/")
+    else
+      conn
+      |> put_flash(:error, "Invalid password")
+      |> render(:login)
+    end
+  end
+
   def login(conn, _params) do
     render(conn, :login)
   end
