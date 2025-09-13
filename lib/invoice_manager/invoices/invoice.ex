92cdfd310a9521f7dbf84f93512f4bb1311d8f2a
Fly Dev <dev@fly.io>
2025-09-13 08:43:27 +0000

    create Invoice schema

---
+++ lib/invoice_manager/invoices/invoice.ex
@@ -0,0 +1,29 @@
+defmodule InvoiceManager.Invoices.Invoice do
+  use Ecto.Schema
+  import Ecto.Changeset
+
+  schema "invoices" do
+    field :name, :string
+    field :is_recurring, :boolean, default: false
+    field :tags, {:array, :string}, default: []
+    field :amount, :decimal
+    field :due_day, :integer
+    field :description, :string
+
+    has_many :monthly_invoices, InvoiceManager.Invoices.MonthlyInvoice
+
+    timestamps(type: :utc_datetime)
+  end
+
+  @doc false
+  def changeset(invoice, attrs) do
+    invoice
+    |> cast(attrs, [:name, :is_recurring, :tags, :amount, :due_day, :description])
+    |> validate_required([:name])
+    |> validate_length(:name, min: 1, max: 255)
+    |> validate_number(:amount, greater_than_or_equal_to: 0)
+    |> validate_number(:due_day, greater_than_or_equal_to: 1, less_than_or_equal_to: 31)
+    |> validate_length(:description, max: 1000)
+    |> unique_constraint(:name)
+  end
+end
